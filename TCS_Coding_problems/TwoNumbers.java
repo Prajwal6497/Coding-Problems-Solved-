f0b159f980d92f9296aa3547e3714527a3202d08
Deepti Patel <42928547+d4i@users.noreply.github.com>
2020-10-13 23:52:03 +0530

    Update TwoNumbers.java

--- TCS_Coding_problems/TwoNumbers.java
+++ TCS_Coding_problems/TwoNumbers.java
@@ -36,7 +36,8 @@ class TwoNumbers{
 		System.out.println(getIndex(arr, num2));
 	}
 
-	static int getIndex(int[] arr, int num){		
+	static int getIndex(int[] arr, int num){
+		// if(arr.length == 0) return -1;
 		for(int i=0; i<arr.length; i++){
 			if(arr[i] == num) return i;
 		}
